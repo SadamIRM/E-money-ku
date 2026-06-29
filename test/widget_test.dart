0a0413e30e194322b301daa44cd2433c37996e28
SadamIRM <sadamirmar@gmail.com>
2026-06-29 11:27:36 +0700

    feat : mereubah nama peckage menajdi smokemoney

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shimmer/main.dart';
 
-import 'package:project/main.dart';
+import 'package:smokemoney/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SmokeMoneyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
